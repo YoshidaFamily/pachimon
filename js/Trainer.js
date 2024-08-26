243b5433e2ef8a004e22a88600e071a4d7faa595
kazuki.yoshida <kazuki.yoshida@zozo.com>
2024-08-26 20:09:38 +0900

    Get monster's status from API

--- js/Trainer.js
+++ js/Trainer.js
@@ -1,8 +1,8 @@
 import { Character } from './Character.js';
 
 export class Trainer extends Character {
-    constructor(scene, name, hp, strength, defense, speed, x, y, texture) {
-        super(scene, name, hp, strength, defense, speed, x, y, texture);
+    constructor(scene, name, x, y, texture) {
+        super(scene, name, 200, 100, 100, 3, x, y, texture);
     }
 
     releaseMonster() {
